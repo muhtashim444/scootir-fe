13eaf5921d252dcb2f47c26fdcee09c94d26350f
muhtashim444 <75309005+muhtashim444@users.noreply.github.com>
2020-12-01 19:30:22 +0500

    Update SignUp.jsx

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -11,7 +11,7 @@ export default function Login() {
 
   const signUp = async () => {
     try {
-      const response = await fetch(`/register`, {
+      const response = await fetch(`https://e-scooter-bk.herokuapp.com//register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
